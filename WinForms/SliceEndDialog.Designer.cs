90304c60e8c953b597fefb9d2d192badbb6795e0
gear_geek <gear2nd.droid@gmail.com>
2025-02-17 16:07:47 +0900

    Add SliceEndDialog
    Upon completion of slicing, the discharge thickness at each XYZ coordinate is graphically displayed for use in adjusting settings.
    スライス完了時に、各XYZ座標における吐出厚みをグラフィカルに表示して、設定の調整に使用できるようにした。

---
+++ WinForms/SliceEndDialog.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace IE_WinForms
+{
+  partial class SliceEndDialog
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+      System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+      System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      this.colorView = new SharpGL.OpenGLControl();
+      this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.colorView)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+      this.splitContainer1.Name = "splitContainer1";
+      this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.colorView);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.chart1);
+      this.splitContainer1.Size = new System.Drawing.Size(600, 845);
+      this.splitContainer1.SplitterDistance = 597;
+      this.splitContainer1.TabIndex = 0;
+      // 
+      // colorView
+      // 
+      this.colorView.DrawFPS = false;
+      this.colorView.Location = new System.Drawing.Point(3, 3);
+      this.colorView.Name = "colorView";
+      this.colorView.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+      this.colorView.RenderContextType = SharpGL.RenderContextType.DIBSection;
+      this.colorView.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+      this.colorView.Size = new System.Drawing.Size(594, 591);
+      this.colorView.TabIndex = 0;
+      this.colorView.MouseDown += new System.Windows.Forms.MouseEventHandler(this.colorView_MouseDown);
+      this.colorView.MouseMove += new System.Windows.Forms.MouseEventHandler(this.colorView_MouseMove);
+      // 
+      // chart1
+      // 
+      chartArea1.Name = "ChartArea1";
+      this.chart1.ChartAreas.Add(chartArea1);
+      legend1.Name = "Legend1";
+      this.chart1.Legends.Add(legend1);
+      this.chart1.Location = new System.Drawing.Point(3, 3);
+      this.chart1.Name = "chart1";
+      series1.ChartArea = "ChartArea1";
+      series1.Legend = "Legend1";
+      series1.Name = "Series1";
+      this.chart1.Series.Add(series1);
+      this.chart1.Size = new System.Drawing.Size(594, 238);
+      this.chart1.TabIndex = 0;
+      this.chart1.Text = "chart1";
+      // 
+      // SliceEndDialog
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(624, 869);
+      this.Controls.Add(this.splitContainer1);
+      this.Name = "SliceEndDialog";
+      this.Text = "SliceEndDialog";
+      this.Load += new System.EventHandler(this.SliceEndDialog_Load);
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.colorView)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.SplitContainer splitContainer1;
+    private SharpGL.OpenGLControl colorView;
+    private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+  }
+}
---
+++ WinForms/SliceEndDialog.cs
@@ -0,0 +1,285 @@
+﻿using SharpGL;
+using SharpGL.SceneGraph.Cameras;
+using SharpGL.SceneGraph.Primitives;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace IE_WinForms
+{
+  public partial class SliceEndDialog : Form
+  {
+    private string fullpath;
+    private List<double> x;
+    private List<double> y;
+    private List<double> z;
+    private List<double> th;
+    private double nozzle;
+    private int preMouseX;
+    private int preMouseY;
+    private int actKeyState;
+    private double maxAngleX = Math.PI - 0.01;
+    private double minAngleX = -Math.PI - 0.01;
+    private double maxAngleZ = Math.PI - 0.01;
+    private double minAngleZ = -Math.PI + 0.01;
+    private double cameraDistance = 150.0;
+    private double cameraAngleX = Math.PI * 1.0 / 4.0;
+    private double cameraAngleZ = 0.0;
+    private double targetPosX = 0.0;
+    private double targetPosY = 0.0;
+    private double targetPosZ = 0.0;
+
+
+    public SliceEndDialog(string fullpath)
+    {
+      InitializeComponent();
+      this.fullpath = fullpath;
+      this.x = new List<double>();
+      this.y = new List<double>();
+      this.z = new List<double>();
+      this.th = new List<double>();
+      this.readCSv();
+      OpenGL gl = this.colorView.OpenGL;
+      this.drawInit();
+      this.drawColorView();
+      this.drawHistogram();
+      //gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+      //gl.LoadIdentity();
+      //gl.Translate(0.0, 0.0, -5.0);
+      //Teapot teapot = new Teapot();
+      //teapot.Draw(gl, 14, 1.0f, OpenGL.GL_FILL);
+      //gl.Flush();
+      //this.fitView();
+      //this.drawColorView();
+    }
+    private static Color hsv2rgb(double h, double s, double v)
+    {
+        double c = v * s;
+        double x = c * (1 - Math.Abs((h / 60) % 2 - 1));
+        double m = v - c;
+
+        double r = 0, g = 0, b = 0;
+
+        if (h >= 0 && h < 60) { r = c; g = x; b = 0; }
+        else if (h >= 60 && h < 120) { r = x; g = c; b = 0; }
+        else if (h >= 120 && h < 180) { r = 0; g = c; b = x; }
+        else if (h >= 180 && h < 240) { r = 0; g = x; b = c; }
+        else if (h >= 240 && h < 300) { r = x; g = 0; b = c; }
+        else if (h >= 300 && h < 360) { r = c; g = 0; b = x; }
+
+        int R = (int)((r + m) * 255);
+        int G = (int)((g + m) * 255);
+        int B = (int)((b + m) * 255);
+
+        return Color.FromArgb(R, G, B);
+    }
+
+    private void readCSv()
+    {
+      this.nozzle = 0.0;
+      using(StreamReader reader = new StreamReader(this.fullpath))
+      {
+        while(!reader.EndOfStream)
+        {
+          string line = reader.ReadLine();
+          string[] buf = line.Split(',');
+          double bx = double.Parse(buf[1]);
+          double by = double.Parse(buf[2]);
+          double bz = double.Parse(buf[3]);
+          double bth = double.Parse(buf[7]);
+          double bnz = double.Parse(buf[8]);
+          this.x.Add(bx);
+          this.y.Add(by);
+          this.z.Add(bz);
+          this.th.Add(bth);
+          this.nozzle += bnz;
+        }
+      }
+      this.nozzle /= this.th.Count;
+    }
+
+    private void drawColorView()
+    {
+      OpenGL gl = this.colorView.OpenGL;
+
+      // point
+      gl.PointSize(1.0f);
+      gl.Begin(OpenGL.GL_POINTS);
+      int cnt = this.th.Count;
+      for(int i = 0; i < cnt; i++)
+      {
+        double bth = this.th[i];
+        if(bth > this.nozzle)
+        {
+          bth = this.nozzle;
+        }
+        double h = 10.0 + 340.0 * bth / this.nozzle;
+        double s = 1.0;
+        double v = 1.0;
+        Color rgb = hsv2rgb(h, s, v);
+        gl.Color(rgb.R, rgb.G, rgb.B);
+        gl.Vertex(this.x[i], this.y[i], this.z[i]);
+      }
+      gl.End();
+      gl.Flush();
+    }
+    private void fitView()
+    {
+      OpenGL gl = this.colorView.OpenGL;
+      double minX = this.x.Min();
+      double maxX = this.x.Max();
+      double minY = this.y.Min();
+      double maxY = this.y.Max();
+      double minZ = this.z.Min();
+      double maxZ = this.z.Max();
+      double cenX = (minX + maxX) / 2.0;
+      double cenY = (minY + maxY) / 2.0;
+      double cenZ = (minZ + maxZ) / 2.0;
+      double sizeX = maxX - minX;
+      double sizeY = maxY - minY;
+      double sizeZ = maxZ - minZ;
+      double maxSize = Math.Max(sizeX, Math.Max(sizeY, sizeZ));
+      double distance = maxSize * 2.0;
+      gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+      gl.LoadIdentity();
+      gl.MatrixMode(OpenGL.GL_PROJECTION);
+      gl.Perspective(45.0, (double)this.colorView.Width / this.colorView.Height, 0.1, distance * 3.0);
+      gl.MatrixMode(OpenGL.GL_MODELVIEW);
+      gl.LookAt(cenX + distance, cenY + distance, cenZ + distance, cenX, cenY, cenZ, 0, 1, 0);
+    }
+
+    private void colorView_MouseMove(object sender, MouseEventArgs e)
+    {
+      int deltaX = e.X - this.preMouseX;
+      int deltaY = e.Y - this.preMouseY;
+      if((Control.MouseButtons & MouseButtons.Left) == MouseButtons.Left)
+      {
+        if((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+        {
+          // with shift & left click
+          double sensitivity = 0.5;
+          this.cameraDistance += deltaY * sensitivity;
+          if (this.cameraDistance < 1.0) this.cameraDistance = 1.0;
+			    if (this.cameraDistance > 500.0) this.cameraDistance = 500.0;
+        }
+        else
+        {
+          // only left click
+          double sensitivity = 0.005;
+			    this.cameraAngleZ += deltaX * sensitivity;
+			    this.cameraAngleX += deltaY * sensitivity;
+			    if (this.cameraAngleX > maxAngleX) this.cameraAngleX = maxAngleX;
+			    if (this.cameraAngleX < minAngleX) this.cameraAngleX = minAngleX;
+			    if (this.cameraAngleZ > maxAngleZ) this.cameraAngleZ = maxAngleZ;
+			    if (this.cameraAngleZ < minAngleZ) this.cameraAngleZ = minAngleZ;
+        }
+      }
+      else if((Control.MouseButtons & MouseButtons.Right) == MouseButtons.Right)
+      {
+        if((Control.ModifierKeys & Keys.Shift) == Keys.Shift)
+        {
+          // with shift & right click
+          double sensitivity = 0.5;
+          this.targetPosZ += deltaY * sensitivity;
+          if (this.targetPosZ < -500.0) this.targetPosZ = -500.0;
+			    if (this.targetPosZ > 500.0) this.targetPosZ = 500.0;
+        }
+        else
+        {
+          // only right click
+			    double sensitivity = 0.5f;
+			    this.targetPosX += deltaX * sensitivity;
+			    this.targetPosY += deltaY * sensitivity;
+			    if (this.targetPosX > 500.0) this.targetPosX = 500.0;
+			    if (this.targetPosX < -500.0) this.targetPosX = -500.0;
+			    if (this.targetPosY > 500.0) this.targetPosY = 500.0;
+			    if (this.targetPosY < -500.0) this.targetPosY = -500.0;
+        }
+      }
+      this.drawInit();
+      this.drawColorView();
+      this.preMouseX = e.X;
+      this.preMouseY = e.Y;
+    }
+
+    private void colorView_MouseDown(object sender, MouseEventArgs e)
+    {
+      this.preMouseX = e.X;
+      this.preMouseY = e.Y;
+    }
+
+    private void drawInit()
+    {
+      OpenGL gl = this.colorView.OpenGL;
+	    gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+	    gl.MatrixMode(OpenGL.GL_PROJECTION);
+	    gl.LoadIdentity();
+      gl.Perspective(45.0, this.colorView.Width / this.colorView.Height, 0.1, 1000.0);
+	    gl.MatrixMode(OpenGL.GL_MODELVIEW);
+	    gl.LoadIdentity();
+      double[] bufCamera = {
+        Math.Sin(this.cameraAngleZ) * Math.Cos(this.cameraAngleX),
+        -Math.Sin(this.cameraAngleX),
+		    Math.Cos(this.cameraAngleZ) * Math.Cos(this.cameraAngleX) };
+      double[] eyePos = {
+        bufCamera[0] * this.cameraDistance + this.targetPosX,
+        bufCamera[1] * this.cameraDistance + this.targetPosY,
+		    bufCamera[2] * this.cameraDistance + this.targetPosZ };
+      gl.LookAt(eyePos[0], eyePos[1], eyePos[2], this.targetPosX, this.targetPosY, this.targetPosZ, 0, 0, 1);
+    }
+
+    private void drawHistogram()
+    {
+      this.chart1.ChartAreas.Clear();
+      ChartArea area = new ChartArea();
+      area.AxisX.LabelStyle.Format = "0.00";
+      this.chart1.ChartAreas.Add(area);
+      Series series = new Series();
+      series.ChartType = SeriesChartType.Column;
+      series.IsValueShownAsLabel = false;
+      series.IsVisibleInLegend = false;
+      this.chart1.Series.Clear();
+      this.chart1.Series.Add(series);
+      this.chart1.Legends.Clear();
+      // histogram
+      int binCnt = 20;
+      double bin = this.nozzle / (double)binCnt;
+      int[] arrCnt = new int[binCnt + 1];
+      int cnt = this.th.Count;
+      for(int i = 0; i < cnt; i++)
+      {
+        int buf = (int)(this.th[i] / bin);
+        if(buf >= binCnt) buf = binCnt;
+        arrCnt[buf] += 1;
+      }
+      for(int i = 0; i < binCnt; i++)
+      {
+        double h = 10.0 + 340.0 * (i + 0.5) * bin / this.nozzle;
+        double s = 1.0;
+        double v = 1.0;
+        Color clr = hsv2rgb(h, s, v);
+        DataPoint point = new DataPoint((i + 0.5) * bin, arrCnt[i]);
+        point.Color = clr;
+        series.Points.Add(point);
+      }
+      Color lastClr = hsv2rgb(360.0, 1.0, 1.0);
+      DataPoint lastPoint = new DataPoint((binCnt + 0.5) * bin, arrCnt[binCnt]);
+      lastPoint.Color = lastClr;
+      series.Points.Add(lastPoint);
+    }
+
+    private void SliceEndDialog_Load(object sender, EventArgs e)
+    {
+
+    }
+  }
+}
